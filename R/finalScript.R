6bc8ee643fc3c66ac1bd320dc37fcbd277b949cb
lillybyrne4 <eab0161@auburn.edu>
2023-11-30 23:47:21 -0600

    Created the package project and moved it to the the correct repo, along w/ the other files Edited description file

--- R/finalScript.R
+++ R/finalScript.R
@@ -1,25 +1,4 @@
 
-#outline for package code from class
-
-set.seed(123)
-x<-matrix(rnorm(600), nrow = 200)
-epsilon <- rnorm(200, 0, sd = 0.25)
-x <- cbind(rep(1, 200),x)
-beta <- c(2, -3, 4, 3) 
-y <- x%*%beta + epsilon
-
-beta_ls <- function(response, predictors, beta) {
-  
-  t(response - predictors%*%beta)%*%(response - predictors%*%beta)
-  
-}
-
-beta_ls(response = y, predictors = x, beta = beta)
-
-optim(rep(0, 4), fn = beta_ls, response = y, predictors = x)
-
-solve(t(x)%*%x)%*%t(x)%*%y
-
 ##########################
 
 library(caret)
@@ -212,7 +191,7 @@ metricplot=function(x,y,metric){
 }
 
 
-data=read.csv("iris_csv.csv")
+data=read.csv("~/Desktop/iris_csv.csv")
 data=data[data$class=="Iris-setosa" | data$class=="Iris-virginica",]
 data$class=ifelse(data$class=="Iris-setosa",1,0)
 
