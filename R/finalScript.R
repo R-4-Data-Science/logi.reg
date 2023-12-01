75750fd8e610e524f68ad802c2f497de86b257a9
lillybyrne4 <eab0161@auburn.edu>
2023-12-01 04:00:32 -0600

    - Added more description for logistic regression function - Reorganized first three functions so that there is one function for the estimates

--- R/finalScript.R
+++ R/finalScript.R
@@ -1,30 +1,58 @@
-
-##########################
-
-library(caret)
-library(boot)
-
-weightInitialization=function(x,y){
-  init_weights=solve(t(x)%*%x)%*%t(x)%*%y
-  return(init_weights)
-}
-
-cost <- function(beta,X,y) {
-  m <- nrow(X)
-  g <- 1/(1+exp(-X%*%beta))
-  (1/m)*sum((-y*log(g)) - ((1-y)*log(1-g)))
-}
-#' model fit
-#' @description estimates logistic regression coefficients and intercept collectively called beta_hat.
-#' @param x a numeric dataset containing explanatory variables
+#' @title Logistic Regression Using Numerical Optimization
+#'
+#' @description Estimates a beta coefficient from a set of explanatory and response variables
+#' with basic logistic regression using numerical optimization. Also displays initial coefficients used for
+#' the regression from the least squares formula.
+#' @param X a numeric dataset containing explanatory variables
 #' @param y a vector containing response variable
-#' @return a list of numeric coefficients 
+#' @return A \code{list} containing the following attributes:
+#' \describe{
+#'      \item{Initial}{Initial coefficients from least squares}
+#'      \item{Estimated}{Estimated coefficients from logistic regression}
+#' }
+#' @importFrom stats
+#' @export
+#' @examples
+#' data=read.csv("iris_csv.csv")
+#' data=data[data$class=="Iris-setosa" | data$class=="Iris-virginica",]
+#' data$class=ifelse(data$class=="Iris-setosa",1,0)
+#' dt = sort(sample(nrow(data), nrow(data)*.7))
+#' train<-data[dt,]
+#' X=as.matrix(train[,1:n_features-1])
+#' y=train[,n_features]
+#' logreg_optim <- function(X, y)
+logreg_optim <- function(X, y) {
 
-logreg_optim=function(x,y){
-  x1 <- cbind(1, x)
-  coeff=optim(par=weightInitialization(x1,y), fn = cost, X=x1,y=y)$par
-  return(coeff)
+  # Calculate initial coefficients from least-squares formula
+  init_weights <- solve(t(X)%*%X)%*%t(X)%*%y
+
+  # Add intercept term to X
+  X <- cbind(1, X)
+
+  # Begin objective function
+  logi_obj <- function(init_weights) {
+
+    # Probability function
+    probs <- 1 / (1 + exp(-X%*%init_weights))
+
+    # Logical regression function
+    cost <- (1/nrow(X))*sum((-y*log(probs)) - ((1-y)*log(1-probs)))
+
+    return(cost)
+  }
+
+  # Optimization using the logistic regression objective function
+  optimal <- optim(par = init_weights, fn = logi_obj, X=X,y=y)
+
+  # Report estimated optimal coefficients
+  est_weights <- optimal$par
+
+  out = list(Initial = init_weights, Estimated = est_weights)
+  out
 }
+
+
+
 #' Bootstrap Confidence Interval
 #' @description calculates Bootstrap confidence intervals
 #' @param x a numeric dataset containing explanatory variables
@@ -38,7 +66,7 @@ logreg_optim=function(x,y){
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' data=read.csv("iris_csv.csv")
 #' data=data[data$class=="Iris-setosa" | data$class=="Iris-virginica",]
 #' data$class=ifelse(data$class=="Iris-setosa",1,0)
@@ -46,11 +74,11 @@ logreg_optim=function(x,y){
 #' train<-data[dt,]
 #' x=as.matrix(train[,1:n_features-1])
 #' y=train[,n_features]
-#' 
+#'
 #' bootstrapCI(x,y,alpha=0.05,n=20)
 #'
 
-bootstrapCI=function(x,y,alpha,n=20){
+bootstrap = function(x,y,alpha,n=20){
   data=data.frame("y"=y,"x"=x)
   n_features=ncol(data)
   beta=matrix(nrow=n,ncol=n_features)
@@ -71,6 +99,29 @@ bootstrapCI=function(x,y,alpha,n=20){
   colnames(CI)=c("Level","Lower","Upper")
   return(CI)
 }
+# Number of boostrap replications
+B <- 500
+
+# Compute the length of vector
+n <- length(student_work)
+
+# Confidence level
+alpha <- 0.05
+
+# Initialisation of
+boot_mean <- rep(NA, B)
+
+# Step 1
+for (i in 1:B){
+  # Step 2
+  student_work_star <- student_work[sample(1:n, replace = TRUE)]
+
+  # Step 3
+  boot_mean[i] <- mean(student_work_star)
+}
+
+# Step 4
+quantile(boot_mean, c(alpha/2, 1 - alpha/2))
 
 #' Logistic curve plot
 #' @description show a visual of logistic regression values against a variable.
@@ -102,29 +153,29 @@ glm_plot<- function(x, y,i)
   x1=x[,i]
   #fit logistic regression model
   coeff <- logreg_optim(x1,y)
-  
+
   #define new data frame that contains predictor variable
   newdata <- data.frame(x=seq(min(x1), max(x1),len=500))
-  
+
   #use fitted model to predict values
   weights=coeff[2]
   intercept=coeff[1]
   pred=as.vector(weights*newdata$x+intercept)
   newdata$y = 1/(1+exp(-pred))
-  
+
   #plot logistic regression curve
   plot(y ~ x1, col="steelblue", main = "Logistic Regression Curve", xlab= "X", ylab = "p")
   lines(y ~ x,newdata)
 }
-#' Confusion Matrix 
-#' @description confusion matrix and relevent metrics with cutoff feature. 
+#' Confusion Matrix
+#' @description confusion matrix and relevent metrics with cutoff feature.
 #' @param y the predicted y values of the test dataset
 #' @param x the numeric y values of the test dataset
 #' @param cutoff the cutoff value which by default is 0.5
-#' @return a list of confusion matrix and metrics such a accuracy, specificity etc. 
+#' @return a list of confusion matrix and metrics such a accuracy, specificity etc.
 #' @export
-#' 
-#' @examples 
+#'
+#' @examples
 #' data=read.csv("iris_csv.csv")
 #' data=data[data$class=="Iris-setosa" | data$class=="Iris-virginica",]
 #' data$class=ifelse(data$class=="Iris-setosa",1,0)
@@ -158,15 +209,15 @@ conf_matrix=function(x,y,cutoff=0.5){
   names(metric_data)=c("Prevalence","Accuracy","Sensitivity","Specificity","False Discovery Rate","Diagnostic Odds Ratio","cutoff")
   return(list(confusion_matrix$table,metric_data))
 }
-#' metric Plot 
-#' @description provides any required metric plot against a range of cutoff values.  
+#' metric Plot
+#' @description provides any required metric plot against a range of cutoff values.
 #' @param y the predicted y values of the test dataset
 #' @param x the numeric y values of the test dataset
-#' @param metric is the name of mr=etric for which the plot is required, like Accuracy. 
-#' @return any required metric plot against a range of cutoff values from 0.1-0.9 
+#' @param metric is the name of mr=etric for which the plot is required, like Accuracy.
+#' @return any required metric plot against a range of cutoff values from 0.1-0.9
 #' @export
-#' 
-#' @examples 
+#'
+#' @examples
 #' data=read.csv("iris_csv.csv")
 #' data=data[data$class=="Iris-setosa" | data$class=="Iris-virginica",]
 #' data$class=ifelse(data$class=="Iris-setosa",1,0)
